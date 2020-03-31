504c9eadf5ca819c230939c4771532a11c0bd035
Peng Chen <pchen@thoughtworks.com>
2020-03-31 17:32:22 +0800

    Add calcReceipts

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,13 @@
-import { printReceipt, validateBarcodes } from '../main'
+import { printReceipt, calcReceipts, validateBarcodes } from '../main'
 
 it('should return false when barcodes is not valid', () => {
     expect(validateBarcodes('0001', '0003', '0005', '009')).toBe(false)
 })
+
+it('should calc receipt correctly when input is valid',() => {
+    expect(calcReceipts('0001', '0003', '0005').length).toBe(3)
+})
+
+// it('should print receipt correctly when input is valid', () => {
+//     expect(printReceipt('0001', '0003', '0005')).toBe('')
+// })
